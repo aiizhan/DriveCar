eb2ac87c8fd377b456caca292730702d3f57c817
Aizhanat <binaainy@gmail.com>
2024-09-30 12:32:05 +0600

    header

--- src/widgets/Header/Header.jsx
+++ src/widgets/Header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../../shared/assets/img/Logo.png'
+import Container from '../../shared/helpers/Container'
+import { NavLink } from 'react-router-dom'
+
 
 const Header = () => {
   return (
-      <div className='text-white'>
-          Header
-    </div>
+    <Container>
+      <div className='text-white flex  justify-between items-center p-6'>
+        <Link to='/' > <img src={Logo} alt="" /> </Link>
+        <div className=' flex gap-7 items-center'>
+          <NavLink to='/' className=' hover:text-[#006AFF] '>Главная</NavLink>
+          <NavLink className=' hover:text-[#006AFF]'>О компании</NavLink>
+          <NavLink className=' hover:text-[#006AFF]'>Услуги</NavLink>
+          <NavLink className=' hover:text-[#006AFF]'>Избранное </NavLink>
+          <NavLink className='border w-[189px] p-1 text-center rounded-lg hover:bg-[#006AFF] hover:border-[#006AFF]'>Регистрация</NavLink>
+        </div>
+
+      </div>
+    </Container>
   )
 }
 
